0b9233df7c81ef52eaf9df863f36990e2398b6dc
meta fighterclub <metafighterclub@protonmail.com>
2022-03-11 15:44:30 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+    ssr:false,
+    meta:{
+        script:[
+          {src:"/game/Build/game.loader.js"}
+        ]
+      }
+})
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+        <canvas id="unity-canvas"   style=" background: red ;width: 800px; height:600px" ></canvas>
+
+  </div>
+</template>
+<script>
+
+export default {
+  mounted(){
+      console.log("ok")
+    createUnityInstance(document.querySelector("#unity-canvas"), {
+        dataUrl: "/game/Build/game.data",
+        frameworkUrl: "/game/Build/game.framework.js",
+        codeUrl: "/game/Build/game.wasm",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "MiniFighterHome",
+        productVersion: "0.1",
+        //matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+         devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+      });
+  }
+}
+</script>
